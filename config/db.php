a123493a985cf8a26ac936accb25c7c192438fb1
sxg <sxg2014@sina.com>
2017-11-07 15:42:09 +0800

    yii shop

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=imooc_shop',
+    'username' => 'yii2_basic',
+    'password' => 'imooc_basic',
+    'charset' => 'utf8',
+    'tablePrefix' => 'shop_',
+];
